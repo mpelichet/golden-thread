9f99e9c265e4a87687a513110b4a7b15de91ece5
Maya Pelichet <mayapelichet@Mayas-MacBook-Pro-6.local>
2018-05-31 15:14:18 +0200

    Edited user object

--- src/pages/charity/charity.ts
+++ src/pages/charity/charity.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CharityprofilePage } from '../charityprofile/charityprofile';
+import { Charity } from '../../models/charity';
 
 /**
  * Generated class for the CharityPage page.
@@ -16,17 +17,19 @@ import { CharityprofilePage } from '../charityprofile/charityprofile';
 })
 export class CharityPage {
 
-  charities: any;
+  public charities: Array<Charity> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.charities = [
-      {name: "Rhino", description: "Testing"},
+    var charity1 = new Charity();
+    charity1.id = 1;
+    charity1.name = "Rhino Poaching";
+    charity1.description = "Save the Rhinos!";
+    charity1.url = "http://www.stoprhinopoaching.com/" ;
+    charity1.image = "https://i.pinimg.com/originals/21/a7/63/21a763d382d95e9eb89f5fb7381e4b06.jpg"; 
+    charity1.donationgoal = 5000; 
 
-      // 'Breadline Africa',
-      // 'Message South Africa',
-      // 'Help the Rural Child',
-      // 'CHOSA South Africa'
-    ]
+    this.charities.push(charity1); 
+
   }
 
   ionViewDidLoad() {
